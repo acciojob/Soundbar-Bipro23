2138d525fb84b91ab6493e5f2346c65980de8902
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-28 19:56:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,20 @@
-//your JS code here. If required.
-let audios = [
-	"applause",
-	"boo",
-	"gasp",
-	"tada",
-	"victory",
-	"wrong",
-]
+let audioPlayer = document.getElementById('audioPlayer');
+let currentAudio = null;
 
-let activeAudio = null;
-
-const button = document.getElementsByClassName(".btn");
-
-function playAudio(){
-    stopAudio();
-	activeAudio.play();
-}
-
-function stopAudio(){
-	if(activeAudio){
-		activeAudio.pause();
-    // activeAudio = null;
-	}
-}
-
-for(let i=0; i<button.length; i++){
-	button[i].addEventListener("click", (event) =>{
-		let innerText = event.target.innerText;
-    
-		if(audios.includes(innerText)){
-
-			let audioElement = document.createElement("audio");
-			audioElement.src = `https://samplelib.com/lib/preview/mp3/sample-12s.mp3`;
-			audioElement.controls = true;
-			activeAudio = audioElement;
-			playAudio();
-		}
-    
-    else{
-      stopAudio();
+function playAudio(soundFileName) {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
     }
-	})
+    audioPlayer.src = `sounds/${soundFileName}`;;
+    audioPlayer.play(); 
+    currentAudio = audioPlayer;
+    
+}
+
+function stopAudio() {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
 }
